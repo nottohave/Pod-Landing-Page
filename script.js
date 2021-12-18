714f896dc9e146cddcbfa93f3e37b0ecaf5ef1ed
nottohave <nottohave@gmail.com>
2021-12-17 22:58:59 -0800

    update

--- script.js
+++ script.js
@@ -2,23 +2,15 @@ const email = document.getElementById("email");
 const form = document.querySelector("#form");
 const emailLabel = document.querySelector(".email-label");
 
+var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-// create event for form to catch any submition event from the btn
-// form.addEventListener("submit", (e) => {
-    // check for all error message
-    let messages = [];
+function validation() {
+    if (email.value.match(pattern)) {
+        console.log("hellow");
+        emailLabel.classList.add("display");
+    } else {
+        console.log("oh no");
+        emailLabel.classList.remove("display");
 
-    // if name is an empty string or empty
-    // if (email.value === "" || email.value == null) {
-    //     emailLabel.classList.add(".display");
-    // } else {
-    //     emailLabel.classList.remove(".display");
-    // }
-
-    // collect all error message
-    // if (messages.length > 0) {
-    //     e.preventDefault()
-        
-    // }
-// })
-
+    }    
+}
