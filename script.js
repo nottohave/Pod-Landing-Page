d1867b06a9f2b0b16144bd2988096dd74ccb404c
nottohave <nottohave@gmail.com>
2021-12-17 22:31:07 -0800

    script.js

--- script.js
+++ script.js
@@ -2,17 +2,23 @@ const email = document.getElementById("email");
 const form = document.querySelector("#form");
 const emailLabel = document.querySelector(".email-label");
 
+
 // create event for form to catch any submition event from the btn
-form.addEventListener("submit", (e) => {
+// form.addEventListener("submit", (e) => {
     // check for all error message
     let messages = [];
 
     // if name is an empty string or empty
-    if (email.value === "" || email.value == null) {
-        emailLabel.toggleAttribute(".display");
-    } else {
-        console.log("submit success");
-    }
-    e.preventDefault()
-})
+    // if (email.value === "" || email.value == null) {
+    //     emailLabel.classList.add(".display");
+    // } else {
+    //     emailLabel.classList.remove(".display");
+    // }
+
+    // collect all error message
+    // if (messages.length > 0) {
+    //     e.preventDefault()
+        
+    // }
+// })
 
